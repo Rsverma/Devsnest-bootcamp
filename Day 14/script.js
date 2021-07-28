20f17b3edce3c06f4b56e8eb92eca10919318b77
Ramesh Verma <rsverma333@gmail.com>
2021-07-28 18:55:22 +0530

    Day 14 THA

---
+++ Day 14/script.js
@@ -0,0 +1,16 @@
+const quoteText = document.getElementById("quote");
+function leftContainer() {
+  quoteText.innerHTML = "Get Back in container";
+}
+function enteredContainer() {
+  quoteText.innerHTML = "That's good";
+}
+function clickedText() {
+  quoteText.innerHTML = "No clicking";
+}
+function leftText() {
+  quoteText.innerHTML = "That's good";
+}
+function enteredText() {
+  quoteText.innerHTML = "Not on the text";
+}
